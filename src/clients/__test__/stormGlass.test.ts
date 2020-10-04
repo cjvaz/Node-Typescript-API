b1b5074e2fed09b635ba92a730ea1f634de6f1b4
Carlos Vaz <carlosjvaz@gmail.com>
2020-10-04 11:07:25 -0300

    api client consumes external data

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,12 +1,20 @@
-import { StormGlass } from '@src/clients/stormGlass'
+import axios from 'axios';
+import { StormGlass } from '@src/clients/stormGlass';
+import stormglassNormalizedResponseFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+import * as stormglassWeatherPointFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+
+jest.mock('axios');
 
 describe('StormGlass client', () => {
-  it('Should return the normalized forecast from the StormGlass service', async () => {
-    const lat = -33.79276;
-    const lng = 151.34343;
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+  it('should return the normalized forecast from the StormGlass service', async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
 
-    const stormGlass = new StormGlass();
+    mockedAxios.get.mockResolvedValue({ data: stormglassWeatherPointFixture });
+
+    const stormGlass = new StormGlass(mockedAxios);
     const response = await stormGlass.fetchPoints(lat, lng);
-    expect(response).toEqual({});
-  })
-})
+    expect(response).toEqual(stormglassNormalizedResponseFixture);
+  });
+});
