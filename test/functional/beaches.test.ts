95225016b31114465c7fe8def617fecf48a217d5
Carlos Vaz <carlosjvaz@gmail.com>
2020-11-13 16:01:21 -0300

    Final - Api Doc with OpenAPI

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,63 +1,135 @@
-import { Beach } from '@src/models/beach';
 import { User } from '@src/models/user';
 import AuthService from '@src/services/auth';
 
-describe('Beaches functional tests', () => {
-  const defaultUser = {
-    name: 'John Doe',
-    email: 'john2@mail.com',
-    password: '1234',
-  };
-
-  let token: string;
+describe('Users functional tests', () => {
   beforeEach(async () => {
-    await Beach.deleteMany({});
     await User.deleteMany({});
-    const user = await new User(defaultUser).save();
-    token = AuthService.generateToken(user.toJSON());
   });
-
-  describe('When creating a new beach', () => {
-    it('should create a beach with success', async () => {
-      const newBeach = {
-        lat: -33.792726,
-        lng: 151.289824,
-        name: 'Manly',
-        position: 'E',
+  describe('When creating a new user', () => {
+    it('should successfully create a new user with encrypted password', async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: '1234',
       };
-
-      const response = await global.testRequest
-        .post('/beaches')
-        .set({ 'x-access-token': token })
-        .send(newBeach);
+      const response = await global.testRequest.post('/users').send(newUser);
       expect(response.status).toBe(201);
-      //Object containing matches the keys and values, even if includes other keys such as id.
-      expect(response.body).toEqual(expect.objectContaining(newBeach));
+      await expect(
+        AuthService.comparePasswords(newUser.password, response.body.password)
+      ).resolves.toBeTruthy();
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          ...newUser,
+          ...{ password: expect.any(String) },
+        })
+      );
     });
 
-    it('should return 422 when there is a validation error', async () => {
-      const newBeach = {
-        lat: 'invalid_string',
-        lng: 151.289824,
-        name: 'Manly',
-        position: 'E',
+    it('Should return a validation error when a field is missing', async () => {
+      const newUser = {
+        email: 'john@mail.com',
+        password: '1234',
       };
-      const response = await global.testRequest
-        .post('/beaches')
-        .set({ 'x-access-token': token })
-        .send(newBeach);
+      const response = await global.testRequest.post('/users').send(newUser);
 
-      expect(response.status).toBe(422);
+      expect(response.status).toBe(400);
       expect(response.body).toEqual({
-        code: 422,
-        error: 'Unprocessable Entity',
-        message:
-          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" at path "lat"',
+        code: 400,
+        error: 'Bad Request',
+        message: 'User validation failed: name: Path `name` is required.',
       });
     });
 
-    it.skip('should return 500 when there is any error other than validation error', async () => {
-      //TODO think in a way to throw a 500
+    it('Should return 409 when the email already exists', async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: '1234',
+      };
+      await global.testRequest.post('/users').send(newUser);
+      const response = await global.testRequest.post('/users').send(newUser);
+
+      expect(response.status).toBe(409);
+      expect(response.body).toEqual({
+        code: 409,
+        error: 'Conflict',
+        message:
+          'User validation failed: email: already exists in the database.',
+      });
+    });
+  });
+
+  describe('when authenticating a user', () => {
+    it('should generate a token for a valid user', async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: '1234',
+      };
+      await new User(newUser).save();
+      const response = await global.testRequest
+        .post('/users/authenticate')
+        .send({ email: newUser.email, password: newUser.password });
+
+      expect(response.body).toEqual(
+        expect.objectContaining({ token: expect.any(String) })
+      );
+    });
+    it('Should return UNAUTHORIZED if the user with the given email is not found', async () => {
+      const response = await global.testRequest
+        .post('/users/authenticate')
+        .send({ email: 'some-email@mail.com', password: '1234' });
+
+      expect(response.status).toBe(401);
+    });
+
+    it('Should return ANAUTHORIZED if the user is found but the password does not match', async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: '1234',
+      };
+      await new User(newUser).save();
+      const response = await global.testRequest
+        .post('/users/authenticate')
+        .send({ email: newUser.email, password: 'different password' });
+
+      expect(response.status).toBe(401);
+    });
+  });
+
+  describe('When getting user profile info', () => {
+    it(`Should return the token's owner profile information`, async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: '1234',
+      };
+      const user = await new User(newUser).save();
+      const token = AuthService.generateToken(user.toJSON());
+      const { body, status } = await global.testRequest
+        .get('/users/me')
+        .set({ 'x-access-token': token });
+
+      expect(status).toBe(200);
+      expect(body).toMatchObject(JSON.parse(JSON.stringify({ user })));
+    });
+
+    it(`Should return Not Found, when the user is not found`, async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: '1234',
+      };
+      //create a new user but don't save it
+      const user = new User(newUser);
+      const token = AuthService.generateToken(user.toJSON());
+      const { body, status } = await global.testRequest
+        .get('/users/me')
+        .set({ 'x-access-token': token });
+
+      expect(status).toBe(404);
+      expect(body.message).toBe('User not found!');
     });
   });
 });
